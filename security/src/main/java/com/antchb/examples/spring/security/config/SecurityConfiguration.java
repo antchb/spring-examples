f078454744e82997bd4456b52282c6ee84c409bd
Anton Chebanenko <106664982+antchb@users.noreply.github.com>
2022-06-29 12:57:25 +0200

    Add Spring Security Module

---
+++ security/src/main/java/com/antchb/examples/spring/security/config/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.antchb.examples.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+// In Spring Security 5.7.0+ the WebSecurityConfigurerAdapter was deprecated. Spring encourages users to move 
+// towards a component-based security configuration
+
+// TODO: UPGRADE to 5.7.0+ style
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // TODO: Replace. Deprecated and unsafe. We use it just for testing 
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+            .withUser(userBuilder.username("admin").password("admin").roles("ADMIN"))
+            .withUser(userBuilder.username("client").password("client").roles("CLIENT"));
+    }
+
+}
---
+++ security/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Spring Examples - Security</title>
+</head>
+<body>
+  <h2>Spring Examples - Security. Main Page</h2>
+  <hr>
+  <a href="/security">Main</a><br/>
+  <hr>
+</body>
+</html>
